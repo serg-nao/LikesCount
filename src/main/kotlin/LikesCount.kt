d674e5e829d69bd4f8ad4ae3bdd701954aef7aed
serg-nao <serg-nao@rambler.ru>
2022-03-29 22:21:11 +0300

    Code changed

--- src/main/kotlin/LikesCount.kt
+++ src/main/kotlin/LikesCount.kt
@@ -1,9 +1,9 @@
 package ru.netology
 
 fun main() {
-    val likes = 101
+    val likes = 111
 
-    val likesCount: String = if (((likes == 1 || likes % 10 == 1) && likes % 100 != 11) && likes != 11) "человеку" else "людям"
+    val likesCount: String = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
 
     println("Понравилось $likes $likesCount")
 
